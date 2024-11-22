59a2948617527a6041e04ab5c07a2165022037ac
Skyla Thadani <skylathadani@microsoft.com>
2024-11-22 08:17:10 -0800

    updated code

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -1,28 +1,24 @@
 using System;
 using System.IO;
-using System.Linq;
+using System.Net;
 using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using System.Runtime.Versioning;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace StaticWebAppsEndToEndTesting.GetMessage
 {
-    public static class GetMessage
+    public class GetMessage
     {
-        [FunctionName("GetMessage")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ExecutionContext context,
-            ILogger log)
+        [Function("GetMessage")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
-            log.LogInformation("C# HTTP trigger function processed a request! :) ");
-            string message = File.ReadAllText(context.FunctionAppDirectory + "/content.txt");
-            return new OkObjectResult(message);
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            string message = File.ReadAllText(Path.Join(Environment.CurrentDirectory, "content.txt"));
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString(message);
+            return response;
         }
     }
 }
