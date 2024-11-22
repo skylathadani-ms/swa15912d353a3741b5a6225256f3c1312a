01c2e2bb749fbf279fafa7836dc1e6d1155ce80d
Skyla Thadani <skylathadani@microsoft.com>
2024-11-22 08:29:54 -0800

    .net 9

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -15,7 +15,8 @@ namespace StaticWebAppsEndToEndTesting.GetMessage
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             var response = req.CreateResponse(HttpStatusCode.OK);
-            string message = File.ReadAllText(Path.Join(Environment.CurrentDirectory, "content.txt"));
+            var azure_root = Environment.GetEnvironmentVariable("HOME") + @"\site\wwwroot";
+            string message = File.ReadAllText(Path.Join(azure_root, "content.txt"));
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
             response.WriteString(message);
             return response;
